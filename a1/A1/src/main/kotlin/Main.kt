493190bd21c37f590cf8af132c22b1919c635217
andysmacstuff <aydna66@gmail.com>
2021-09-25 01:54:44 -0400

    add button functionalities

--- a1/A1/src/main/kotlin/Main.kt
+++ a1/A1/src/main/kotlin/Main.kt
@@ -1,10 +1,15 @@
 import javafx.application.Application
 import javafx.scene.Scene
 import javafx.scene.control.*
+import javafx.scene.image.Image
+import javafx.scene.image.ImageView
 import javafx.scene.input.KeyCode
 import javafx.scene.layout.BorderPane
+import javafx.scene.layout.HBox
+import javafx.scene.layout.VBox
 import javafx.stage.Stage
 import java.io.File
+import java.io.IOException
 
 
 class Main : Application()  {
@@ -26,6 +31,19 @@ class Main : Application()  {
         return fileNames
     }
 
+    @Throws(IOException::class)
+    fun isFileNameValid(filename: String?): Boolean {
+        val file = File(filename)
+        var created = false
+        return try {
+            created = file.createNewFile()
+            created
+        } finally {
+            if (created) {
+                file.delete()
+            }
+        }
+    }
 
     override fun start(stage: Stage) {
 
@@ -34,15 +52,52 @@ class Main : Application()  {
         val layout = BorderPane()
 
         // top: menubar
-        val menuBar = MenuBar()
+        val menuBar1 = MenuBar()
         val fileMenu = Menu("File")
         val fileNew = MenuItem("New")
 
-        menuBar.menus.add(fileMenu)
+        menuBar1.menus.add(fileMenu)
         fileMenu.items.add(fileNew)
 
+        val homeIcon = ImageView(Image("homeIcon.png", 15.0, 15.0, false, false))
+        val homeButton = Button("Home")
+        homeButton.graphic = homeIcon
+
+        val prevIcon = ImageView(Image("prev.png", 15.0, 15.0, false, false))
+        val prevButton = Button("Previous")
+        prevButton.graphic = prevIcon
+
+        val nextIcon = ImageView(Image("next.png", 15.0, 15.0, false, false))
+        val nextButton = Button("Next")
+        nextButton.graphic = nextIcon
+
+        val delIcon = ImageView(Image("delete.png", 15.0, 15.0, false, false))
+        val delButton = Button("Delete")
+        delButton.graphic = delIcon
+
+        val renameIcon = ImageView(Image("rename.png", 15.0, 15.0, false, false))
+        val renameButton = Button("Rename")
+        renameButton.graphic = renameIcon
+
+        val moveIcon = ImageView(Image("move.png", 15.0, 15.0, false, false))
+        val moveButton = Button("Move")
+        moveButton.graphic = moveIcon
+
+        val topContainer = VBox()  //Creates a container to hold all Menu Objects.
+        val buttonContainer = HBox() // for buttons layout
+
+        buttonContainer.children.add(homeButton)
+        buttonContainer.children.add(prevButton)
+        buttonContainer.children.add(nextButton)
+        buttonContainer.children.add(delButton)
+        buttonContainer.children.add(renameButton)
+        buttonContainer.children.add(moveButton)
+
+        topContainer.children.add(menuBar1)
+        topContainer.children.add(buttonContainer)
+
         // added stuff
-        var currDir: File = File("${System.getProperty("user.dir")}/test/")
+        var currDir = File("${System.getProperty("user.dir")}/test/")
         var currPath: String = currDir.absolutePath
 
         var statusBar1 = Label(currPath)
@@ -53,7 +108,7 @@ class Main : Application()  {
         }
 
         // left: tree
-        var fileTree: ListView<String> = getFiles(currDir)
+        val fileTree: ListView<String> = getFiles(currDir)
 
         /*
         fileTree.selectionModel.selectedItemProperty().addListener(
@@ -157,8 +212,200 @@ class Main : Application()  {
             }
         }
 
+        // button click events (menu)
+
+        homeButton.setOnMouseClicked {
+            // take us home to the test directory
+            currDir = File("${System.getProperty("user.dir")}/test/")
+            currPath = currDir.absolutePath
+            statusBar1 = Label(currPath)
+            layout.bottom = statusBar1
+
+            // update fileTree
+            fileTree.items.clear()
+            val tmpDir = getFiles(currDir)
+            for (file in tmpDir.items) {
+                fileTree.items.add(file)
+            }
+            layout.left = fileTree // update view
+        }
+
+        nextButton.setOnMouseClicked {
+
+            if (!fileTree.selectionModel.isEmpty) {
+                val currItem = fileTree.selectionModel.selectedIndex
+                val currFile = currDir.listFiles().sorted()[currItem]
+                println(currFile)
+
+                // Update directory
+                if (currFile.isDirectory) {
+                    currDir = currFile
+
+                    // update fileTree
+                    fileTree.items.clear()
+                    val tmpDir = getFiles(currFile)
+                    for (file in tmpDir.items) {
+                        fileTree.items.add(file)
+                    }
+                    layout.left = fileTree // update view
+
+                }
+            }
+        }
+
+        prevButton.setOnMouseClicked {
+            if (currDir.parent != null) {
+                currDir = currDir.parentFile //move to parent folder
+                currPath = currDir.absolutePath
+                statusBar1 = Label(currPath) // update status bar
+                layout.bottom = statusBar1
+
+                // update fileTree
+                fileTree.items.clear()
+                val tmpDir = getFiles(currDir)
+                for (file in tmpDir.items) {
+                    fileTree.items.add(file)
+                }
+                layout.left = fileTree // update view
+            }
+        }
+
+        renameButton.setOnMouseClicked {
+            if (!fileTree.selectionModel.isEmpty) {
+                val dialog = TextInputDialog()
+                dialog.title = "Rename File"
+                dialog.contentText = "Please enter the new file name:"
+
+                val result = dialog.showAndWait()
+                if (result.isPresent && isFileNameValid(result.get())) {
+
+                    val currItem = fileTree.selectionModel.selectedIndex
+                    val currFile = currDir.listFiles().sorted()[currItem]
+                    println(currFile)
+
+                    val tmpFile = File("${currDir.absolutePath}/${result.get()}")
+
+
+                    currFile.renameTo(tmpFile) // rename
+
+                    // update view
+                    currPath = currFile.absolutePath
+                    statusBar1 = Label(currPath) // update status bar
+                    layout.bottom = statusBar1
+
+                    fileTree.items.clear()
+                    val tmpDir = getFiles(currDir)
+                    for (file in tmpDir.items) {
+                        fileTree.items.add(file)
+                    }
+                    layout.left = fileTree // update view
+
+
+                } else {
+                    val alertDialog = Alert(Alert.AlertType.ERROR)
+                    alertDialog.title = "Invalid Operation"
+                    alertDialog.contentText = "Cannot rename file"
+                    alertDialog.show()
+                }
+            }
+
+        }
+
+
+        delButton.setOnMouseClicked {
+
+            if (!fileTree.selectionModel.isEmpty) {
+
+                // test if selected file is valid to be deleted
+                val currItem = fileTree.selectionModel.selectedIndex
+                val currFile = currDir.listFiles().sorted()[currItem]
+                if (currFile.parentFile.canWrite()) {
+
+                    val dialog = Alert(Alert.AlertType.CONFIRMATION)
+                    dialog.title = "Confirmation of Deletion"
+                    dialog.headerText = "Deleting selected file"
+                    dialog.contentText = "Are you sure?"
+
+                    val result = dialog.showAndWait()
+                    if (result.get() == ButtonType.OK) {
+                        // delete
+                        currFile.delete()
+
+                        //update directory
+                        // update view
+                        currPath = currDir.absolutePath
+                        statusBar1 = Label(currPath) // update status bar
+                        layout.bottom = statusBar1
+
+                        fileTree.items.clear()
+                        val tmpDir = getFiles(currDir)
+                        for (file in tmpDir.items) {
+                            fileTree.items.add(file)
+                        }
+                        layout.left = fileTree // update view
+                    }
+                }
+                else {
+                    val alertDialog = Alert(Alert.AlertType.ERROR)
+                    alertDialog.title = "Invalid Operation"
+                    alertDialog.contentText = "Cannot delete file"
+                    alertDialog.show()
+                }
+            }
+        }
+
+        moveButton.setOnMouseClicked {
+
+            if (!fileTree.selectionModel.isEmpty) {
+                val currItem = fileTree.selectionModel.selectedIndex
+                val currFile = currDir.listFiles().sorted()[currItem]
+
+                val dialog = TextInputDialog()
+                dialog.title = "Move File"
+                dialog.contentText = "Please specify new location path:"
+
+                val result = dialog.showAndWait()
+
+                if (result.isPresent && File(result.get()).isDirectory && currFile.canWrite()) {
+                    //val newPath1 = result.get() + currFile.name
+                    val newPath2 = result.get() + "/" + currFile.name
+
+                    // is valid move
+                    println(newPath2)
+
+                    currFile.renameTo(File(newPath2))
+
+
+                    // update view
+                    currPath = currDir.absolutePath
+                    statusBar1 = Label(currPath) // update status bar
+                    layout.bottom = statusBar1
+
+                    fileTree.items.clear()
+                    val tmpDir = getFiles(currDir)
+                    for (file in tmpDir.items) {
+                        fileTree.items.add(file)
+                    }
+                    layout.left = fileTree // update view
+                }
+                else {
+                    val alertDialog = Alert(Alert.AlertType.ERROR)
+                    alertDialog.title = "Invalid Operation"
+                    alertDialog.contentText = "Cannot move file"
+                    alertDialog.show()
+                }
+
+            }
+        }
+
+
+
+
+
+
         // build the scene graph
-        layout.top = menuBar
+
+        layout.top = topContainer
         layout.left = fileTree
         layout.bottom = statusBar1
 
